d7cf2f6c9acd52c0440cb8ab401169e35d2b2316
YiguiDing <2449695354@qq.com>
2022-01-17 08:40:46 +0800

    v 1.3 服务端异常处理

--- nodeApp/index.js
+++ nodeApp/index.js
@@ -5,10 +5,11 @@ const ignoreFiles=["index.js","package.json","package-lock.json"]
 
 var http = require('http');
 var fs =require("fs");
+const { log } = require('console');
 
 var basePath=process.cwd();
 var basePath_404=process.cwd()+"/404.html";
-
+var errLogFilePath=process.cwd() + "/log";
 var server=http.createServer(
     function (request, response)
     {
@@ -24,15 +25,24 @@ var server=http.createServer(
             {
                 filePath = basePath + request.url;
             }
-            fs.readFile(filePath,function(err,data){
-                if(err)
+            fs.readFile(filePath,function(filePath_err,data){//读取所请求的页面
+                if(filePath_err)//所请求的页面读取出错
                 {
-                    console.log("文件读取错误:"+err);
+                    console.log("文件读取错误:"+filePath_err);
                     console.log("用户请求的url为:"+request.url);
-                    console.log("发送个用户文件为:"+basePath_404);
                     response.statusCode=404;
-                    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
-                    response.end(fs.readFileSync(basePath_404));
+                    fs.readFile(basePath_404,function(basePath_404_err,data){////读取404的页面
+                        if(basePath_404_err)//404页面读取出错
+                        {
+                            response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+                            response.end(basePath_404_err);
+                        }else
+                        {
+                            response.setHeader('Content-Type', 'text/html; charset=utf-8');
+                            // response.end(data+filePath_err);
+                            response.end(data);
+                        }
+                    });
                 }else
                 {
                     console.log("文件读取成功");
@@ -49,6 +59,17 @@ var server=http.createServer(
         
     }
 );
+process.on('uncaughtException', err => {//捕捉未处理的报错
+    console.log('uncaughtException err:')
+    console.error(err && err.stack)
+    fs.writeFile(errLogFilePath,err,function(err){
+        if(err)
+            console.log("记录出错日志失败");
+        else
+            console.log("记录出错日志成功");
+
+    });
+  });
 server.listen(port,hostname,()=>{
         console.log('Server running at ' + protocol + hostname + ':' + port + '/')
     }
